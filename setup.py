fbcb9a5a6618ca6d4b4150a357f313aecf704396
Alex-98jeweler <zubakin.alexandr98@gmail.com>
2023-04-06 12:20:39 +0700

    edit setup

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
-# you are being empty, while we are developing you
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst', 'r') as file:
+        content = file.read()
+    return content
+
+
+setup(
+    name='dasha_mail',
+    version='1.0.0',
+    author='Dashamail',
+    author_email='support@dashamail.ru',
+    description='Dasha Mail SDK',
+    long_description=readme(),
+    url='https://github.com/Alex-98jeweler/dasha_mail/',
+    packages=find_packages('src'),
+    package_dir={"": "src"},
+    install_requires=['requests'],
+    zip_safe=False,
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires='>=3.8',
+)
