30a2c219421eaf7c30856a5147a17e9522d95acb
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-06-27 18:20:20 +0700

    edit test configuration

--- test/test_configuration.py
+++ test/test_configuration.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from dasha_mail import dasha_api_client
@@ -8,8 +9,7 @@ class TestConfiguration(unittest.TestCase):
 
     def setUp(self) -> None:
         self.api_client = dasha_api_client.DashaApiClient()
-        configuration.Configuration.configure("047dc2e62b1694e88aeca1da2dc87837")
+        configuration.Configuration.configure(os.getenv("DASHA_API_TOKEN"))
 
     def test_configured(self):
         data = self.api_client.send_request('lists.get')
-        print(data)
