40570b7fe12fde26d40d08958c88d06065567e2c
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-06-28 18:29:44 +0700

    change version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='dasha_mail',
-    version='1.0.3',
+    version='1.0.4',
     author='Dashamail',
     author_email='support@dashamail.ru',
     description='Dasha Mail SDK',
